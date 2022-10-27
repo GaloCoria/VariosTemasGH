e9f54d306ef340c3b7c446d26001c4acb1f71bd0
Android Studio <devandroid@ort>
2022-10-27 08:55:17 -0300

    aa

--- screens/Principal.js
+++ screens/Principal.js
@@ -7,48 +7,56 @@ export default function Principal(){
         <View style={styles.container}>
         
         <Button
+        style={styles.Boton}
         title="Clima"
         onPress={() => {
           navigation.navigate('Clima')
         }}
       />
        <Button
+       style={styles.Boton}
         title="Accelerometer"
         onPress={() => {
           navigation.navigate('Accelerometer')
         }}
       />
        <Button
+       style={styles.Boton}
         title="Camerafondo"
         onPress={() => {
           navigation.navigate('Camerafondo')
         }}
       />
        <Button
+       style={styles.Boton}
         title="Contactos"
         onPress={() => {
           navigation.navigate('Contactos')
         }}
       />
        <Button
+       style={styles.Boton}
         title="ImagePicker"
         onPress={() => {
           navigation.navigate('ImagePicker')
         }}
       />
       <Button
+      style={styles.Boton}
         title="QR"
         onPress={() => {
           navigation.navigate('QR')
         }}
       />
        <Button
+       style={styles.Boton}
         title="Videoplay"
         onPress={() => {
           navigation.navigate('Videoplay')
         }}
       />
       <Button
+      style={styles.Boton}
         title="NumeroEmergencia"
         onPress={() => {
           navigation.navigate('NumeroEmergencia')
@@ -62,5 +70,7 @@ const styles = StyleSheet.create({
     container: {
       marginTop: 50,
     },
-   
+   Boton:{
+    marginTop:20
+   }
   });
